aec40a56e596c0d7d628dc5877a0995c4ede208b
AgusKhaer02 <aguskkhaer@gmail.com>
2024-11-21 11:26:44 +0700

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,7 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3525236809"
+    serviceWorkerVersion: "801203723"
   }
 });
+
